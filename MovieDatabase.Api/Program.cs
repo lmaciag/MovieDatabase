f3887c24e500913a49efd3d303e27453f8c3b0ac
Łukasz Maciąg <kontakt@lukaszmaciag.pl>
2023-12-06 07:43:37 +0100

    Add NLog with appsettings.json configuration

--- MovieDatabase.Api/Program.cs
+++ MovieDatabase.Api/Program.cs
@@ -1,11 +1,15 @@
 using MovieDatabase.Application;
 using MovieDatabase.Infrastructure;
+using NLog.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
+
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
 
 var app = builder.Build();
 
